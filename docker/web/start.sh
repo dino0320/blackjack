e8209a1ef4cdb1423def4014aff1a8a32e49bdd5
dino0320 <127201712+dino0320@users.noreply.github.com>
2024-06-07 11:36:01 +0900

    Node.jsをインストールする処理を追加

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -8,5 +8,13 @@ cd $PROJECT_PATH
 
 php artisan migrate
 
+# nvmが読み込まれていないため読み込む
+set +x
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+set -x
+
+npm ci
+
 php-fpm
 nginx -g "daemon off;"
