bdd69d1bef0ffb65b34c6c4f8368a0860da2b7da
dino0320 <127201712+dino0320@users.noreply.github.com>
2024-06-08 15:13:57 +0900

    viteの設定追加

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/blackjack.css',
+                'resources/js/app.js',
+                'resources/js/blackjack.js',
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
