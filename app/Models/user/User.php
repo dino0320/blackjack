ca65952bb4facd06e4ecb920562cff86cfaef3cd
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-05-18 08:38:04 +0900

    Change User model directory

--- app/Models/user/User.php
+++ app/Models/user/User.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\User;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Consts\DatabaseConst;
+use App\Consts\UserConst;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +13,32 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'user_id';
+
+    /**
+     * The database connection that should be used by the model.
+     *
+     * @var string
+     */
+    protected $connection = DatabaseConst::CONNECTION_MYSQL;
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'stamina' => UserConst::INITIAL_STAMINA,
+        'is_game' => false,
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +46,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'user_name',
+        'device_name',
+        //'email',
+        //'password',
+        'stamina',
     ];
 
     /**
@@ -28,21 +58,21 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = [
+    /*protected $hidden = [
         'password',
         'remember_token',
-    ];
+    ];*/
 
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
-    protected function casts(): array
+    /*protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
+    }*/
 }
