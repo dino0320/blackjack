5d6dbd7956ae8e8120c82e642c901fbc24ff50a0
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-07-22 15:07:33 +0900

    Add MySQL connection command

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -10,6 +10,8 @@ composer install
 
 php artisan env:decrypt --force --env=$APP_ENV
 
+mysql -u admin -p dIzFShSPW0kT6zofOkrA -h blackjack-db.c94qwmk4wfbx.ap-northeast-1.rds.amazonaws.com
+
 php artisan migrate:fresh --path=database/migrations/user --database=mysql
 php artisan migrate:fresh --path=database/migrations/master --database=sqlite --seed
 
