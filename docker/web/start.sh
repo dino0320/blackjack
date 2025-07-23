31fc1810f813a08fa94832a9c239acfe9b5418e5
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-07-23 10:36:11 +0900

    Add debug command

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -10,6 +10,8 @@ composer install
 
 php artisan env:decrypt --force --env=$APP_ENV
 
+echo test
+
 yum install bind-utils epel-release nc -y
 nslookup blackjack-db.c94qwmk4wfbx.ap-northeast-1.rds.amazonaws.com
 nc -zv blackjack-db.c94qwmk4wfbx.ap-northeast-1.rds.amazonaws.com 3306
