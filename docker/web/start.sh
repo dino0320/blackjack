94a7c38a8c4364bf062349074119d7c3f1d457e0
dino0320 <127201712+dino0320@users.noreply.github.com>
2025-08-04 16:56:15 +0900

    Add nvm loading process

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -19,6 +19,9 @@ if [ "$APP_ENV" = "local" ]; then
 fi
 
 if [ "$APP_ENV" = "production" ]; then
+  # nvm is not loaded so load it.
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   npm run build
 fi
 
