82687f5ed02e411387830314dc11ebfa4d113110
dino0320 <127201712+dino0320@users.noreply.github.com>
2026-02-22 15:56:57 +0900

    Fix permissions

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -25,8 +25,9 @@ php artisan migrate:fresh --force --path=database/migrations/user --database=mys
 php artisan migrate:fresh --force --path=database/migrations/master --database=sqlite --seed
 
 # Give permissions for log output etc.
-chmod 775 "$PROJECT_PATH/storage/logs"
-chmod 775 "$PROJECT_PATH/storage/framework/views"
+chmod 775 "$PROJECT_PATH/storage"
+#chmod 775 "$PROJECT_PATH/storage/logs"
+#chmod 775 "$PROJECT_PATH/storage/framework/views"
 
 # Create a ready flag
 touch /tmp/app-ready
